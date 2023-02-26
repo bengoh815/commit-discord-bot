56f7f58cc42492de16bb4cc0d0e2c01b9f9f9692
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2023-02-25 23:24:04 -0600

    Got simple message up and working
    Bugs: not running on server but DM works
    Need to work on pinging and running on a loop
    Need to put in on replit and run for infinite
    Future features idea, have a streak counter for users

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randint
 import discord
 import os
 from dotenv import load_dotenv
@@ -14,6 +15,16 @@ async def on_message(message):
         return
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+    if message.content == "$test":
+        await message.channel.send(pingMessage())
+
+
+def greetings():
+    greetings = ["Hello", "Howdy", "Bonjour", "Heya", "Wassup"]
+    return greetings[randint(0, 4)]
+
+def pingMessage():
+    return greetings() + " coders, remember to do you commits today!"
 
 load_dotenv()
 client.run(os.getenv('TOKEN'))
